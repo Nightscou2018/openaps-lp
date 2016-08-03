c5290ee6e37098cd384262432858d5a9f95d8838
Brandon Faloona <brandon@faloona.net>
2016-08-03 12:32:37 -0700

    Update next version of do-loop.sh

--- do-loop-new.sh
+++ do-loop-new.sh
@@ -3,12 +3,6 @@ PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/
 
 echo do-loop-new.sh Started
 
-function error_exit
-{
-        echo "$1" 1>&2
-        exit 1
-}
-
 cd /home/pi/openaps-lp
 
 logger -t do-loop-start "OPENAPS-LP LOOP START"
@@ -22,10 +16,10 @@ if oref0 fix-git-corruption; then
                 sleep 5
         done
 
-        (openaps preflight || error_exit "Preflight Failed.") 2>&1 | logger -t do-loop-preflight
-        openaps gather-clean-data 2>&1 | logger -t do-loop-gather               
+        (openaps preflight || exit 99) 2>&1 | logger -t do-loop-preflight
+        openaps gather-clean-data 2>&1 | logger -t do-loop-gather
         openaps do-oref0 2>&1 | logger -t do-loop-predict
         openaps enact-oref0 2>&1 | logger -t do-loop-enact
-        openaps report-nightscout 2>&1 | logger -t do-loop-status
+        (openaps get-basal-status && openaps monitor-pump-history && openaps report-nightscout) 2>&1 | logger -t do-loop-status
 
 fi
