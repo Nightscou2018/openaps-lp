10f4c7579fa119f98dd79ac6c469fed9f2ca5e45
Brandon Faloona <brandon@faloona.net>
2016-08-03 23:56:22 -0700

    openaps-report invoke raw-pump/pump-history-raw.json monitor/pump-history.json
          TODO: better change descriptions
          /usr/local/bin/openaps-report invoke raw-pump/pump-history-raw.json monitor/pump-history.json

--- do-loop-new.sh
+++ do-loop-new.sh
@@ -8,11 +8,9 @@ function error_exit
 {
 	END=`date +%s`
 	ELAPSED=$(( $END - $START ))
-
 	echo "$1" 1>&2
-	logger -t do-loop "$1"
-	logger -t do-loop-end "OPENAPS-LP LOOP ERROR ($ELAPSED seconds)"
-
+	logger -t do-loop-error "LOOP FAIL $1"
+	logger -t do-loop-end "OPENAPS-LP LOOP ERROR EXIT ($ELAPSED seconds)"
         exit 1
 }
 
@@ -20,19 +18,35 @@ cd /home/pi/openaps-lp
 
 logger -t do-loop-start "OPENAPS-LP LOOP START"
 
-if oref0 fix-git-corruption; then # 2>&1 > >(logger -t do-loop-start)
+if ! grep -q max settings/settings.json; then
+	{ openaps get-settings || error_exit "get-settings"; } 2>&1 > >(logger -t do-loop-start)
+fi
 
+if oref0 fix-git-corruption; then # 2>&1 > >(logger -t do-loop-start)
 	logger -t do-loop-start "PREFLIGHT"
+	preflight_timeout=$((SECONDS+120))
         until openaps preflight; # 2>&1 > >(logger -t do-loop-start)
         do
-                sleep 10
-		logger -t do-loop-start "Waiting (openaps preflight failed)"
+                if [ $SECONDS -gt $preflight_timeout ]; then
+			error_exit "PREFLIGHT TIMEOUT ERROR"
+		fi
+		sleep 10
+		logger -t do-loop-start "PREFLIGHT WAITING"
         done
 
-        { openaps gather-clean-data || error_exit "LOOP FAIL gather-clean-data"; } 2>&1 > >(logger -t do-loop-gather)
-        { openaps do-oref0 || error_exit "LOOP FAIL do-oref0"; } 2>&1 > >(logger -t do-loop-predict)
-        { openaps enact-oref0 || error_exit "LOOP FAIL enact-oref0"; } 2>&1 > >(logger -t do-loop-enact)
-	{ openaps report-nightscout || error_exit "LOOP FAIL report-nightscout"; } 2>&1 > >(logger -t do-loop-status)
+	# Update if missing
+	if ! grep -q maxBasal settings/settings.json; then
+        	logger -t do-loop-start "GET PUMP SETTINGS"
+		{ openaps get-settings || error_exit "get-settings"; } 2>&1 > >(logger -t do-loop-start)
+	fi
+
+        # Main loop
+	{ openaps gather-clean-data || error_exit "gather-clean-data"; } 2>&1 > >(logger -t do-loop-gather)
+        { openaps do-oref0 || error_exit "do-oref0"; } 2>&1 > >(logger -t do-loop-predict)
+        { openaps enact-oref0 || error_exit "enact-oref0"; } 2>&1 > >(logger -t do-loop-enact)
+
+	# Update nightscout
+	{ openaps report-nightscout || error_exit "report-nightscout"; } 2>&1 > >(logger -t do-loop-status)
 fi
 
 END=`date +%s`
