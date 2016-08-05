d04ae174c65f188504f28c2589bf688149603cc9
Brandon Faloona <brandon@faloona.net>
2016-08-05 13:21:45 -0700

    Refactor scripts/git-clone.sh

--- scripts/git-reclone.sh
+++ scripts/git-reclone.sh
@@ -6,8 +6,9 @@ logger -t git-reclone "GIT RECLONE START"
 ##
 # Paths in this script are relative to home direcotry
 #
+
 cd /home/pi
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]; then
         logger -t git-reclone "GIT RECLONE FAIL - Home dir."
         exit 1
 fi
@@ -20,13 +21,13 @@ mkdir $NEWDIR |& logger -t git-reclone
 mv openaps-lp/* $NEWDIR  |& logger -t git-reclone
 
 rm -rf openaps-lp |& logger -t git-reclone
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]; then
         logger -t git-reclone "GIT RECLONE FAIL - rm openaps-lp"
         exit 1
 fi
 
 git clone https://github.com/bfaloona/openaps-lp.git |& logger -t git-reclone
-if [ $? -ne 0 ]
+if [ $? -ne 0 ]; then
         logger -t git-reclone "GIT RECLONE FAIL - git clone"
         exit 1
 fi
@@ -35,9 +36,9 @@ cp openaps-lp-assets/pump.ini openaps-lp/ |& logger -t git-reclone
 cd openaps-lp
 
 git status > /dev/null
-if [ $? -eq 0 ]
+if [ $? -eq 0 ]; then
 	logger -t git-reclone "GIT RECLONE END"
-then
+else
 	logger -t git-reclone "GIT RECLONE FAIL - git status"
 	exit 1
 fi
