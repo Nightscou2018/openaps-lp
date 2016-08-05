16edd2dcb3b5e5979576acefbf72d807072a90e5
Brandon Faloona <brandon@faloona.net>
2016-08-05 12:12:01 -0700

    Refactor reclone script

--- scripts/git-reclone.sh
+++ scripts/git-reclone.sh
@@ -10,4 +10,11 @@ mv openaps-lp openaps-lp_corrupt/openaps-lp_recent  | logger -t git-reclone
 git clone https://github.com/bfaloona/openaps-lp.git | logger -t git-reclone
 cp openaps-lp-assets/pump.ini openaps-lp/ | logger -t git-reclone
 cd openaps-lp
+
 git status > /dev/null
+if [ $? -eq 0 ]
+	logger -t git-reclone "GIT RECLONE END"
+then
+	logger -t git-reclone "GIT RECLONE FAILURE"
+	exit 1
+fi
