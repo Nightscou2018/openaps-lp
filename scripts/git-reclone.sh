76a1d805f2bb39c54051d14abd7ad9e2f2e8fd1c
Brandon Faloona <brandon@faloona.net>
2016-08-05 10:00:33 -0700

    openaps-report invoke monitor/clock.json monitor/clock-zoned.json
          TODO: better change descriptions
          /usr/local/bin/openaps-report invoke monitor/clock.json monitor/clock-zoned.json

--- scripts/git-reclone.sh
+++ scripts/git-reclone.sh
@@ -1,4 +1,13 @@
 SHELL=/bin/bash
 PATH=/home/pi/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 
-(cd /home/pi && rm -rf openaps-lp_corrupt && mv openaps-lp openaps-lp_corrupt && git clone https://github.com/bfaloona/openaps-lp.git && cp openaps-lp_corrupt/pump.ini openaps-lp/) 2>&1 | logger -t git-reclone
+logger -t git-reclone "GIT RECLONE START"
+
+cd /home/pi
+rm -rf openaps-lp_corrput/openaps-lp_recent | logger -t git-reclone
+mkdir openaps-lp_corrput/openaps-lp_recent |logger -t git-reclone
+mv openaps-lp openaps-lp_corrupt/openaps-lp_recent  | logger -t git-reclone
+git clone https://github.com/bfaloona/openaps-lp.git | logger -t git-reclone
+cp openaps-lp-assets/pump.ini openaps-lp/ | logger -t git-reclone
+cd openaps-lp
+git status > /dev/null
